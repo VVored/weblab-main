95c96ccb4ae67a381dcecdc48ea8118545a6df28
VVored <muchvova-a@mail.ru>
2023-05-16 21:33:04 +0500

    фывфывцфывцфыв

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -4,7 +4,7 @@ import Menu from '../components/UI/menu/Menu';
 import Navbar from '../components/UI/navbar/Navbar';
 import { useContext } from 'react';
 import { MenuContext } from '../context';
-import macbookAsabi from '../img/Macbook Pro 01.png';
+import macbookSicilia from '../img/Macbook Pro 01.png';
 
 export default function Portfolio() {
     const { setIsOpenMenu } = useContext(MenuContext);
@@ -28,13 +28,13 @@ export default function Portfolio() {
                             <div className="arrowBlack"></div>
                         </div>
                     </div>
-                    <div className="portElementImgs">
-                        <img className='portImgAsabi' src={macbookAsabi} alt="" />
+                    <div className="portElementImgs siciliaImg">
+                        <img className='portImgAsabi' src={macbookSicilia} alt="" />
                     </div>
                 </div>
                 <div className="portElement">
-                    <div className="portElementImgs">
-                        <img className='portImgAsabi' src={macbookAsabi} alt="" />
+                    <div className="portElementImgs asabiImg">
+                        <img className='portImgAsabi' src={macbookSicilia} alt="" />
                     </div>
                     <div className="portElementTxt">
                         <h2>Разработка сайта ресторана  <span style={{ color: '#F6343B' }}>«Asabi»</span></h2>
@@ -42,6 +42,38 @@ export default function Portfolio() {
                             Ресторан нетривиальной средиземноморской кухни, завоевавший любовь гостей еще в начале нулевых, в марте 2018 года ресторан получил новый интерьер и концепцию.
                         </p>
                         <p>Разработка современного и адаптивного под мобильные устройства сайта на основе фирменного стиля ресторана. Возможность оплаты блюд онлайн.</p>
+                        <div style={{ display: 'flex', marginTop: 60, alignItems: 'center' }}>
+                            <a href="/">Посмотреть кейс</a>
+                            <div className="arrowBlack"></div>
+                        </div>
+                    </div>
+                </div>
+                <div className="portElement">
+                    <div className="portElementTxt">
+                        <h2>Разработка сайта  <span style={{ color: '#1840A7' }}>Челябинского цинкового завода</span></h2>
+                        <p>
+                            Выпуск сайта Челябинского Цинкового завода, крупнейшего российского предприятия по производству цинка и сплавов на его основе.
+                            Фирменный стиль, структура и мобильная версия сайта.
+                        </p>
+                        <div style={{ display: 'flex', marginTop: 60, alignItems: 'center' }}>
+                            <a href="/">Посмотреть кейс</a>
+                            <div className="arrowBlack"></div>
+                        </div>
+                    </div>
+                    <div className="portElementImgs cincImg">
+                        <img className='portImgAsabi' src={macbookSicilia} alt="" />
+                    </div>
+                </div>
+                <div className="portElement">
+                    <div className="portElementImgs asabiImg">
+                        <img className='portImgAsabi' src={macbookSicilia} alt="" />
+                    </div>
+                    <div className="portElementTxt shebImg">
+                        <h2>Разработка сайта для <span style={{ color: '#2A80B9' }}>Щебеночного завода</span></h2>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repudiandae adipisci minima, fuga praesentium dolore doloribus voluptatibus maiores consequatur molestias ad modi illo nostrum earum culpa quod at officia ea ipsam.
+                        </p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus similique odit, in doloribus recusandae officia velit inventore rerum maiores iusto! Enim fuga quasi ipsa neque rerum dolorem molestiae tempore ipsum.</p>
                         <div style={{ display: 'flex', marginTop: 60, alignItems: 'center' }}>
                             <a href="/">Посмотреть кейс</a>
                             <div className="arrowBlack"></div>
